0b67f421e678e2e7f47c6464a37a6d7907b0cef5
faresrahmeni97 <faresrahmeni97@gmail.com>
2021-03-07 15:53:39 +0100

    user crud + des classes vides

--- src/main/java/com/example/App_formation/entities/User.java
+++ src/main/java/com/example/App_formation/entities/User.java
@@ -1,4 +1,61 @@
 package com.example.App_formation.entities;
 
-public class User {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+public class User implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="usercode")
+    private int code;
+    private String login;
+    private int password;
+
+/*    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLES", joinColumns = {
+            @JoinColumn(name = "USER_ID") }, inverseJoinColumns = {
+            @JoinColumn(name = "ROLE_ID") })
+    private Set<Role> roles;
+
+        public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+ */
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public void seConnecter(){
+
+    }
 }
