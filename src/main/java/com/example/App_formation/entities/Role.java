7277c1676f8fde3214bc593b3dd8923da3f71479
faresrahmeni97 <faresrahmeni97@gmail.com>
2021-03-07 16:24:47 +0100

    role crud

--- src/main/java/com/example/App_formation/entities/Role.java
+++ src/main/java/com/example/App_formation/entities/Role.java
@@ -1,4 +1,33 @@
 package com.example.App_formation.entities;
 
-public class Role {
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Role implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+  private int id;
+    @Column
+  private int nom;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNom() {
+        return nom;
+    }
+
+    public void setNom(int nom) {
+        this.nom = nom;
+    }
 }
