7e9698ec91ff868a847a03bebbe216d7160a0cbc
faresrahmeni97 <faresrahmeni97@gmail.com>
2021-03-07 14:52:35 +0100

    Initial commit

---
+++ src/main/java/com/example/App_formation/AppFormationApplication.java
@@ -0,0 +1,13 @@
+package com.example.App_formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFormationApplication.class, args);
+	}
+
+}
